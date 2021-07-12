f851b3dac5bf18c121c9eca7184192cc8703a9dd
ilariamaci94 <87028581+ilariamaci94@users.noreply.github.com>
2021-07-12 17:35:47 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -7,9 +7,19 @@ setwd("C:/lab/")
 library(raster)
 
 #inseirire i dati della cartella lab all' interno di R
-#brick importa l' immagine satellitare e assegnare un nome 
+#brick importa l' immagine satellitare
+#con la freccia si assegna un nome all' oggetto, il file è esterno ad R e si usano le virgolette
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 #vedere le caratteristiche del file
 p224r63_2011
 #vedere le bande con plot
 plot(p224r63_2011)
+#cambio della scala dei colori(stabiliamo la variazione dei colori che sono numeri che rappresentano la riflettanza in una certa lunghezza d'onda)
+#con la c prima della parenesi si indica un vettore, il numero 100 indica i livelli di colore
+#si inserisce la freccia per dare un nome alla color scale
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+#si plotta l' immagine con la scala di colori che abbiamo creato
+plot(p224r63_2011, col=cl)
+#nuova scala di colori
+cl <- colorRampPalette(c("red","orange","pink","blue","yellow")) (100)
+plot(p224r63_2011, col=cl)
