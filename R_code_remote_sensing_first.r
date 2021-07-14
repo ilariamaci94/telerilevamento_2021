fcedb2fd97eaa7312974eeeca9614e5a8c7ca424
ilariamaci94 <87028581+ilariamaci94@users.noreply.github.com>
2021-07-14 20:42:22 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -14,6 +14,8 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd")
 p224r63_2011
 #vedere le bande con plot
 plot(p224r63_2011)
+
+#Day 2
 #cambio della scala dei colori(stabiliamo la variazione dei colori che sono numeri che rappresentano la riflettanza in una certa lunghezza d'onda)
 #con la c prima della parenesi si indica un vettore, il numero 100 indica i livelli di colore
 #si inserisce la freccia per dare un nome alla color scale
@@ -23,3 +25,40 @@ plot(p224r63_2011, col=cl)
 #nuova scala di colori
 cl <- colorRampPalette(c("red","orange","pink","blue","yellow")) (100)
 plot(p224r63_2011, col=cl)
+
+#Day 3
+#richiamare il pacchetto e i dati della cartella che utilizziamo
+library (raster)
+setwd("C:/lab/")
+# si associa il nome e si richima l' immagine
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+#si plotta l' immagine con la scala di colori assegnata 
+cl <- colorRampPalette(c("red","orange","pink","blue","yellow")) (100)
+plot(p224r63_2011, col=cl)
+
+#Bande Landsat
+#B1: banda del blu
+#B2: banda del verde
+#B3: banda del rosso
+#B4: infrarosso vicino
+#B5: infrarosso medio
+#B6: infrarosso lontano (termico)
+#B7: infrarosso medio
+
+#si plotta la banda del blu
+#dev.off ripulisce la finestra grafica
+dev.off()
+#nome della banda blu è B1_sre vista dalle informazioni 
+#si spiega al sistema che vogliamo solo l' immagine della banda B1_sre e che si lega con il $ all' immagine satellitare
+plot(p224r63_2011$B1_sre)
+#plottare la banda 1 con una diversa scala di colori
+cls <- colorRampPalette(c("red","orange","pink","blue","yellow")) (100)
+plot(p224r63_2011, col=cls)
+# si chiude la grafica
+dev.off()
+#si utilizza par per inserire le bande che vogliamo noi, è una funzione generica per fare un settaggio dei parametri di un certo grafico da creare
+#si plotta il grafico con la banda del blu e quella del verde accanto si fa un multifrime (mf)->par, altrimenti sovrascriverebbe
+#par inserisce le immagini in una sola riga e due colonne, si si vogliono due righe e una colonna si invertono i numeri in parentesi
+par(mfrow=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
