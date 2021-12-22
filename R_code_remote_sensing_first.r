3f90e7ad2ef7e10c3653328749a52de68ed2677e
ilariamaci94 <87028581+ilariamaci94@users.noreply.github.com>
2021-12-22 17:28:27 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -144,4 +144,33 @@ plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
 
-
+#Day 5
+#si richiama il pacchetto raster da utilizzare
+library(raster)
+#settaggio working directory
+setwd("C:/lab/")
+#carico l'immagine utilizzata precedentemente
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+#si inizia con l' analisi multitemporale caricando l'immagine relativa al 1988
+#brick importa un intero set di bande
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+p224r63_1988
+#si plotta l' intera immagine che rappresenta le singole bande in vare lunghezze d'onda
+plot(p224r63_1988)
+#si plotta l' immagine in RGB in scala naturale 
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+#plotto l'immagine in RGB con l' infrarosso
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+#plotto la stessa immagine per il 1988 e per il 2011 insieme 
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+#plotto le immagini sia con lo stretch lineare che histogram con rappresentazione quadrata
+pdf("multitemp.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+dev.off()
+#si installa il pacchetto in R "RStoolbox"
