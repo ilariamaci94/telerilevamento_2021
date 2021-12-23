339dc4a1861f0f3a21fac1475321337f3a5921f2
ilariamaci94 <87028581+ilariamaci94@users.noreply.github.com>
2021-12-23 14:05:33 +0100

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,45 +1,48 @@
-time series analysis
+#time series analysis
 #Greenland increase of temperature
 #Data and code from Emanuela Cosma
 
+#install.packages("raster")
 #richiamo il pacchetto raster
 library(raster)
-#richiamo pacchetto rasterVis
-library(rasterVis)
 #settaggio wd
-setwd("C:/lab/greenland")
-#carichiamo il primo dei file del set, sono singoli dati. Non si usa brick ma raster
-#raster:carichiamo singoli dati, importiamo il primo
+setwd("C:/lab/Greenland")
+
+#importiamo i dati all'interno della cartella che rappresentano la stima della temperatura "lst" che deriva da Copernicus;
+#carichiamo i dati singolarmente con la funzione "raster" 
 lst_2000 <- raster("lst_2000.tif")
-#possiamo plottarlo
+#plottiamo il primo dataset
 plot(lst_2000)
-#procediamo con il file successivo
+#procediamo con il caricamento dei file successivi
 lst_2005 <- raster("lst_2005.tif")
 plot(lst_2005)
-#anche gli ultimi due
 lst_2010 <- raster("lst_2010.tif")
 lst_2015 <- raster("lst_2015.tif")
-#mettiamo insieme i plot
+#si plottano i 4 dataset in una configurazione quadrata
 par(mfrow=c(2,2))
 plot(lst_2000)
 plot(lst_2005)
 plot(lst_2010)
 plot(lst_2015)
-#vogliamo importare i file tutti insieme invece di usare raster e importarli uno per volta, usiamo quindi lapply
-#lapply: applicare la funzione raster a una lista di file (rlist), nel nostro caso i file lst
-#la list.files: crea la lista che R utilizzerà per applicare la funzione lapply
+
+#per importare le immagini tutte insieme si utilizza lapply
+#lapply: applico la funzione raster a una lista di file (rlist), nel nostro caso i file lst
+#list.files: crea la lista di file che R utilizzerà per applicare la funzione lapply
 list.files()
-#con pattern ricerchiamo i file che ci servono in base alle caratteristiche comuni nel nome,come il fatto che tutti si chiamano lst
+#con pattern ricerchiamo i file che ci servono in base alle caratteristiche comuni nel nome (in questo caso lst)
 rlist <- list.files(pattern="lst")
+#si visualizza in contenuto
 rlist
-#lapply: x=lista dove applicare la funzione FUN= funzione raster da applicare (il tutto si applica a tutti i file lst)
+#lapply: x=lista dove applicare la funzione FUN= funzione raster da applicare
+#applichiamo il raster con lapply a tutta la lista creata ("rlist")
 import <- lapply(rlist,raster)
+#visualizzo le caratteristiche dei file della lista importati
 import
-#ora possiamo creare un unico pacchetto di file con tutti quelli importati, lo facciamo con la funzione stack
-#stack function: abbiamo una lista di file raster e li mettiamo tutti insieme
+#si crea un unico pacchetto con tutti i file raster importati utilizzando la funzione "stack"
 TGr <- stack(import) 
-#ci servirà per ottenere il plot di tutti i file insieme
+#serve per ottenere il plot di tutti i file insieme per facilitare la visualizzazione
 plot(TGr)
+
 #immagini sovrapposte con schema RGB
 plotRGB(TGr, 1, 2, 3, stretch="Lin")
 plotRGB(TGr, 2, 3, 4, stretch="Lin") #plot con valori della temperatura
