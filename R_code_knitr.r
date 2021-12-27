bb9259a942760e5695f12ecd8588ced638fa1694
ilariamaci94 <87028581+ilariamaci94@users.noreply.github.com>
2021-12-27 12:34:39 +0100

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,12 @@
+#R_code_knitr.r
+#inserire all'interno di uno stesso file pdf più immagini
+#creare un vero report di codice e immagini
+
+#settaggio wd
+setwd("C:/lab/")
+
+#installo il pacchetto 
+install.packages("knitr")
+
+#richiamo il pacchetto con require
+require(knitr)
