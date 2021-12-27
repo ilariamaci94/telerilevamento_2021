dd04d8e55e005c90f911e7112c1ac7dd8b0d7096
ilariamaci94 <87028581+ilariamaci94@users.noreply.github.com>
2021-12-27 14:32:46 +0100

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -10,4 +10,5 @@ setwd("C:/lab/")
 
 #richiamo il pacchetto
 library(knitr)
+#con "stitch" si utilizza uno script di R generando un pdf in versione di report, si enera in maniera automatica
 stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
