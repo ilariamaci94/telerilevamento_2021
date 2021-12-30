875802b668604ac8797d3dd76733097e68a444a6
ilariamaci94 <87028581+ilariamaci94@users.noreply.github.com>
2021-12-30 19:22:59 +0100

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -20,26 +20,28 @@ pairs(p224r63_2011)
 
 
 #PCA ANALISI IMPATTANTE quindi si ricampiona il dato in modo che sia più leggero con la funzione Aggregate, cambiamo quindi la risoluzione iniziale 
-#richiamare le librerie e cartelle di riferimento
+#richiamare le librerie e seleziono la cartella di riferimento
 library(raster)
 library (RStoolbox)
-setwd("D:/lab/")
+setwd("C:/lab/")
+#carico l'immagine con brick per avere l'intero set di dati
 p224r63_2011 <-brick ("p224r63_2011_masked.grd")
-#aggregate: resampling o ricampionamento, aggreghiamo le celle, il fact è il fattore di ingrandimento del pixel, fun serve ad aggregare i valori 
-p224r63_2011res <- aggregate(p224r63_2011, fact=10)
-#facciamo il plot per visualizzare l'immagine e li mettiamo a confronto
+#aggreghiamo i pixel ad esempio con una media per ottenere una risoluzione più bassa
+#aggregate:ricampionamento, aggreghiamo le celle, il fact è il fattore di ingrandimento del pixel
+p224r63_2011res <- aggregate(p224r63_2011, fact=10) #si passa da un pixel di 30 metri a uno di 300 metri
+#facciamo il plot per visualizzare l'immagine con risoluzione minore e la mettiamo a confronto con quella originale
 par(mfrow=c(2,1))
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
 plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin")
-#la funzione per fare la PCA, dove prendiamo i dati originali e facciamo passare un asse per i dati di variabilità maggiore e uno per il minore
-#rasterPCA prende il pacchetto di dati e lo compatta in un numero minore di bande
+#per utilizzare la funzione PCA si prendono i dati originali e si fa passare un asse lungo la variabilità maggiore e un altro asse lungo la variabilità minore
+#rasterPCA: prende il pacchetto di dati e lo compatta in un numero minore di bande
 p224r63_2011res_pca <- rasterPCA(p224r63_2011res)
-#vediamo la varianza utilizzando la funzione summary, che è molto generica e base,dà un sommario del nostro modello
-summary(p224r63_2011res_pca$model) #così vediamo la varianza nelle varie bande
+#vediamo la varianza delle bande utilizzando la funzione summary, che fornisce un sommario del modello
+summary(p224r63_2011res_pca$model) #si lega la mappa al modello
 #con plot, prendo l'immagine modello la unisco alla mappa
-plot(p224r63_2011res_pca$map)
+plot(p224r63_2011res_pca$map) #la prima componente ci dà l'informazione mentre l'ultima è solo rumore
 p224r63_2011res_pca
-#plottiamo quindi in rgb tutta l'immagine
+#plottiamo quindi in RGB tutta l'immagine
 plotRGB(p224r63_2011res_pca$map, r=1,g=2,b=3, stretch="lin") #vediamo un'immagine che utilizza tutte e tre le componenti principali
-#per ottenere informazioni più complesse del file utilizzo la funzione str (structure)
+#per ottenere informazioni più complesse del file utilizzo la funzione "str" (structure)
 str(p224r63_2011res_pca)
